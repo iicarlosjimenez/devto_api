ead960e98ed1974a3e40d6b5fd93d88f752efe61
Carlos Jiménez <iicarlosjim@gmail.com>
2024-03-23 22:36:41 -0600

    feat: Endpoint Users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,7 @@ Route::get('/', function () {
         'status' => true
     ]);
 });
+Route::get('/users', [UserController::class, 'index']);
 Route::get('/articulos/{filters?}', [ArticuloController::class, 'index']);
 Route::post('/articulos', [ArticuloController::class, 'store']);
 Route::put('/articulos/{id}', [ArticuloController::class, 'update']);
